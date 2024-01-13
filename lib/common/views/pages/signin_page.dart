14997c45c5d9c4ea99656e7d7ce6aebc03f17809
ChandulaJ <114409648+ChandulaJ@users.noreply.github.com>
2024-01-13 11:09:29 +0530

    Sign in and signup pages designed

--- lib/common/views/pages/signin_page.dart
+++ lib/common/views/pages/signin_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg_provider/flutter_svg_provider.dart';
 import 'package:get/get.dart';
 import 'package:harvest_delivery/common/views/components/farmerCustomerSelector.dart';
 import 'package:harvest_delivery/customerSide/view/pages/main_page.dart';
@@ -21,130 +22,139 @@ class _SignInPageState extends State<SignInPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       resizeToAvoidBottomInset: false,
-      body: Center(
-        child: SingleChildScrollView(
-          child: Padding(
-            padding: const EdgeInsets.all(20.0),
-            child: Column(
-              mainAxisAlignment:MainAxisAlignment.spaceAround,
-
-              children: [
-
-                // Title
-                Text(
-                  "Welcome Back",
-                  style: TextStyle(
-                    color: Colors.blue,
-                    fontWeight: FontWeight.bold,
-                    fontSize: 30,
-                  ),
-                ),
-                const SizedBox(height: 30.0),
-                SingleUserSelector(),
-
-                const SizedBox(height: 30.0),
-
-                // Email
-                TextFormField(
-                  decoration: const InputDecoration(
-                    border: InputBorder.none,
-                    focusedBorder: OutlineInputBorder(
-                      borderSide: BorderSide(color: Colors.blue),
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: Svg(
+              'lib/common/views/images/signin_background.svg',
+              size: Size(10, 10),
+            ),
+            fit: BoxFit.fill,
+          ),
+        ),
+        child: Center(
+          child: SingleChildScrollView(
+            child: Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  // Title
+                  Text(
+                    "Welcome Back",
+                    style: TextStyle(
+                      color: Colors.blue,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 30,
                     ),
-                    filled: true,
-                    fillColor: Color(0xFFF1F4FF),
-                    labelText: 'E-mail',
                   ),
-                ),
+                  const SizedBox(height: 30.0),
+                  SingleUserSelector(),
 
-                const SizedBox(height: 16),
+                  const SizedBox(height: 30.0),
 
-                // Password
-                TextFormField(
-                  obscureText: _obscurePassword,
-                  decoration: InputDecoration(
-                    border: InputBorder.none,
-                    focusedBorder: OutlineInputBorder(
-                      borderSide: BorderSide(color: Colors.blue),
+                  // Email
+                  TextFormField(
+                    decoration: const InputDecoration(
+                      border: InputBorder.none,
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.blue),
+                      ),
+                      filled: true,
+                      fillColor: Colors.white,
+                      labelText: 'E-mail',
                     ),
-                    filled: true,
-                    fillColor: Color(0xFFF1F4FF),
-                    labelText: 'Password',
-                    suffixIcon: IconButton(
-                        icon: Icon(_obscurePassword
-                            ? Icons.visibility_off
-                            : Icons.visibility),
-                        onPressed: () {
-                          setState(() {
-                            _obscurePassword = !_obscurePassword;
-                          });
-                        }),
                   ),
-                ),
 
-                const SizedBox(height: 16),
+                  const SizedBox(height: 16),
 
-                // forgot password
-                Row(
-                  children: [
-                    Spacer(),
-                    GestureDetector(
-                      onTap: () {
-                        //TODO: implement forgot password page
-                        print("Forgot Password");
+                  // Password
+                  TextFormField(
+                    obscureText: _obscurePassword,
+                    decoration: InputDecoration(
+                      border: InputBorder.none,
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.blue),
+                      ),
+                      filled: true,
+                      fillColor: Colors.white,
+                      labelText: 'Password',
+                      suffixIcon: IconButton(
+                          icon: Icon(_obscurePassword
+                              ? Icons.visibility_off
+                              : Icons.visibility),
+                          onPressed: () {
+                            setState(() {
+                              _obscurePassword = !_obscurePassword;
+                            });
+                          }),
+                    ),
+                  ),
+
+                  const SizedBox(height: 16),
+
+                  // forgot password
+                  Row(
+                    children: [
+                      Spacer(),
+                      GestureDetector(
+                        onTap: () {
+                          //TODO: implement forgot password page
+                          print("Forgot Password");
+                        },
+                        child: const Text(
+                          'Forgot Password?',
+                        ),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(height: 16),
+
+                  // Button
+                  Container(
+                    width: MediaQuery.of(context).size.width,
+                    child: ElevatedButton(
+                      onPressed: () {
+                        Get.to(() => MainPage());
                       },
-                      child: const Text(
-                        'Forgot Password?',
+                      style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.blue,
+                          foregroundColor: Colors.white,
+                          padding: EdgeInsets.all(16.0),
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(8.0))),
+                      child: Text(
+                        'Sign In',
+                        style: TextStyle(
+                            fontSize: 18.0, fontWeight: FontWeight.normal),
                       ),
-                    ),
-                  ],
-                ),
-                const SizedBox(height: 16),
-
-                // Button
-                Container(
-                  width: MediaQuery.of(context).size.width,
-                  child: ElevatedButton(
-                    onPressed: () {
-                      Get.to(() => MainPage());
-                    },
-                    style: ElevatedButton.styleFrom(
-                        backgroundColor: Colors.blue,
-                        foregroundColor: Colors.white,
-                        padding: EdgeInsets.all(16.0),
-                        shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(8.0))),
-                    child: Text(
-                      'Sign In',
-                      style: TextStyle(
-                          fontSize: 18.0, fontWeight: FontWeight.normal),
                     ),
                   ),
-                ),
 
-                const SizedBox(height: 16),
+                  const SizedBox(height: 16),
 
-                // Text
-                RichText(
-                    text: TextSpan(
-                        text: "Don't have an account? ",
-                        style: TextStyle(
-                          color: Colors.black54,
+                  // Text
+                  RichText(
+                      text: TextSpan(
+                          text: "Don't have an account? ",
+                          style: TextStyle(
+                            color: Colors.black54,
+                          ),
+                          children: [
+                        TextSpan(
+                          text: 'Sign Up',
+                          style: TextStyle(
+                            color: Colors.blue,
+                          ),
+                          recognizer: TapGestureRecognizer()
+                            ..onTap = () {
+                              // Navigate to the Sign In screen
+                              Get.to(() => SignUpPage());
+                            },
                         ),
-                        children: [
-                      TextSpan(
-                        text: 'Sign Up',
-                        style: TextStyle(
-                          color: Colors.blue,
-                        ),
-                        recognizer: TapGestureRecognizer()
-                          ..onTap = () {
-                            // Navigate to the Sign In screen
-                            Get.to(() => SignUpPage());
-                          },
-                      ),
-                    ])),
-              ],
+                      ])),
+                ],
+              ),
             ),
           ),
         ),
--- lib/common/views/pages/signup_page.dart
+++ lib/common/views/pages/signup_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg_provider/flutter_svg_provider.dart';
 import 'package:harvest_delivery/common/views/pages/signin_page.dart';
 
 import 'package:sign_in_button/sign_in_button.dart';
@@ -20,225 +21,202 @@ class _SignUpPageState extends State<SignUpPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       resizeToAvoidBottomInset: false,
-      body: Center(
-        child: SingleChildScrollView(
-          child: Padding(
-            padding: const EdgeInsets.all(20.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                // Title
-                Text(
-                  "Create Account",
-                  style: TextStyle(
-                    color: Colors.blue,
-                    fontWeight: FontWeight.bold,
-                    fontSize: 30,
-                  ),
-                ),
-
-                // tagline
-                Text("Create an account to locate your hope with us ..."),
-
-                const SizedBox(height: 16),
-
-                // Email
-                TextFormField(
-                  decoration: const InputDecoration(
-                    border: InputBorder.none,
-                    focusedBorder: OutlineInputBorder(
-                      borderSide: BorderSide(color: Colors.blue),
-                    ),
-                    filled: true,
-                    fillColor: Color(0xFFF1F4FF),
-                    labelText: 'E-mail',
-                  ),
-                ),
-
-                const SizedBox(height: 16),
-
-                // Password
-                TextFormField(
-                  obscureText: _obscurePassword,
-                  decoration: InputDecoration(
-                    border: InputBorder.none,
-                    focusedBorder: OutlineInputBorder(
-                      borderSide: BorderSide(color: Colors.blue),
-                    ),
-                    filled: true,
-                    fillColor: Color(0xFFF1F4FF),
-                    labelText: 'Password',
-                    suffixIcon: IconButton(
-                        icon: Icon(_obscurePassword
-                            ? Icons.visibility_off
-                            : Icons.visibility),
-                        onPressed: () {
-                          setState(() {
-                            _obscurePassword = !_obscurePassword;
-                          });
-                        }),
-                  ),
-                ),
-
-                const SizedBox(height: 16),
-
-                // Confirm Password
-                TextFormField(
-                  obscureText: _obscureConfirmPassword,
-                  decoration: InputDecoration(
-                    border: InputBorder.none,
-                    focusedBorder: OutlineInputBorder(
-                      borderSide: BorderSide(color: Colors.blue),
-                    ),
-                    filled: true,
-                    fillColor: Color(0xFFF1F4FF),
-                    labelText: 'Confirm Password',
-                    suffixIcon: IconButton(
-                        icon: Icon(_obscureConfirmPassword
-                            ? Icons.visibility_off
-                            : Icons.visibility),
-                        onPressed: () {
-                          setState(() {
-                            _obscureConfirmPassword = !_obscureConfirmPassword;
-                          });
-                        }),
-                  ),
-                ),
-
-                const SizedBox(height: 16),
-
-                // Button
-                Container(
-                  width: MediaQuery.of(context).size.width,
-                  child: ElevatedButton(
-                    onPressed: () {
-                      // Implement your button logic here
-                    },
-                    style: ElevatedButton.styleFrom(
-                        backgroundColor: Colors.blue,
-                        foregroundColor: Colors.white,
-                        padding: EdgeInsets.all(16.0),
-                        shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(8.0))),
-                    child: Text(
-                      'Sign Up',
-                      style: TextStyle(
-                          fontSize: 18.0, fontWeight: FontWeight.normal),
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: Svg(
+              'lib/common/views/images/signin_background.svg',
+              size: Size(10, 10),
+            ),
+            fit: BoxFit.fill,
+          ),
+        ),
+        child: Center(
+          child: SingleChildScrollView(
+            child: Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  // Title
+                  Text(
+                    "Create Account",
+                    style: TextStyle(
+                      color: Colors.blue,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 30,
                     ),
                   ),
-                ),
+                  const SizedBox(height: 30.0),
 
-                const SizedBox(height: 16),
+                  // tagline
+                  Text("Create an account to locate your hope with us ..."),
 
-                // Text
-                RichText(
-                    text: TextSpan(
-                        text: 'Already have an account? ',
-                        style: TextStyle(
-                          color: Colors.black54,
-                        ),
-                        children: [
-                      TextSpan(
-                        text: 'Sign In',
-                        style: TextStyle(
-                          color: Colors.blue,
-                        ),
-                        recognizer: TapGestureRecognizer()
-                          ..onTap = () {
-                            // TODO: Implement the Sign In logic here
-                            // Navigate to the Sign In screen
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                builder: (context) => SignInPage(),
-                              ),
-                            );
-                          },
+                  const SizedBox(height: 20.0),
+
+                  // Email
+                  TextFormField(
+                    decoration: const InputDecoration(
+                      border: InputBorder.none,
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.blue),
                       ),
-                    ])),
+                      filled: true,
+                      fillColor: Colors.white,
+                      labelText: 'E-mail',
+                    ),
+                  ),
 
-                const SizedBox(height: 16),
+                  const SizedBox(height: 16),
 
-                // Divider with line
-                Container(
-                  width: MediaQuery.of(context).size.width,
-                  child: const Row(
+                  // Password
+                  TextFormField(
+                    obscureText: _obscurePassword,
+                    decoration: InputDecoration(
+                      border: InputBorder.none,
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.blue),
+                      ),
+                      filled: true,
+                      fillColor: Colors.white,
+                      labelText: 'Password',
+                      suffixIcon: IconButton(
+                          icon: Icon(_obscurePassword
+                              ? Icons.visibility_off
+                              : Icons.visibility),
+                          onPressed: () {
+                            setState(() {
+                              _obscurePassword = !_obscurePassword;
+                            });
+                          }),
+                    ),
+                  ),
+
+                  const SizedBox(height: 16),
+
+                  // Confirm Password
+                  TextFormField(
+                    obscureText: _obscureConfirmPassword,
+                    decoration: InputDecoration(
+                      border: InputBorder.none,
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.blue),
+                      ),
+                      filled: true,
+                      fillColor: Colors.white,
+                      labelText: 'Confirm Password',
+                      suffixIcon: IconButton(
+                          icon: Icon(_obscureConfirmPassword
+                              ? Icons.visibility_off
+                              : Icons.visibility),
+                          onPressed: () {
+                            setState(() {
+                              _obscureConfirmPassword = !_obscureConfirmPassword;
+                            });
+                          }),
+                    ),
+                  ),
+
+                  const SizedBox(height: 20.0),
+
+                  // Button
+                  Container(
+                    width: MediaQuery.of(context).size.width,
+                    child: ElevatedButton(
+                      onPressed: () {
+                        // Implement your button logic here
+                      },
+                      style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.blue,
+                          foregroundColor: Colors.white,
+                          padding: EdgeInsets.all(16.0),
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(8.0))),
+                      child: Text(
+                        'Sign Up',
+                        style: TextStyle(
+                            fontSize: 18.0, fontWeight: FontWeight.normal),
+                      ),
+                    ),
+                  ),
+
+                  const SizedBox(height: 20.0),
+
+                  // Text
+                  RichText(
+                      text: TextSpan(
+                          text: 'Already have an account? ',
+                          style: TextStyle(
+                            color: Colors.black54,
+                          ),
+                          children: [
+                        TextSpan(
+                          text: 'Sign In',
+                          style: TextStyle(
+                            color: Colors.blue,
+                          ),
+                          recognizer: TapGestureRecognizer()
+                            ..onTap = () {
+                              // TODO: Implement the Sign In logic here
+                              // Navigate to the Sign In screen
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) => SignInPage(),
+                                ),
+                              );
+                            },
+                        ),
+                      ])),
+
+                  const SizedBox(height: 16),
+
+
+                  // Checkbox with Terms of Service and Privacy Policy
+                  Row(
                     children: [
-                      Expanded(
-                        child: Divider(
-                          color: Colors.black54,
-                        ),
-                      ),
-                      Padding(
-                        padding: EdgeInsets.symmetric(horizontal: 8.0),
-                        child:
-                            Text("OR", style: TextStyle(color: Colors.black54)),
+                      Checkbox(
+                        value: _agreedToTerms,
+                        onChanged: (bool? value) {
+                          setState(() {
+                            _agreedToTerms = value ?? false;
+                          });
+                        },
                       ),
                       Expanded(
-                        child: Divider(
-                          color: Colors.black54,
+                        child: GestureDetector(
+                          onTap: () {
+                            print("Show Policies");
+                          },
+                          child: RichText(
+                            text: const TextSpan(
+                              text: 'I agree to the ',
+                              style: TextStyle(color: Colors.black),
+                              children: [
+                                TextSpan(
+                                  text: 'Terms of Service',
+                                  style: TextStyle(
+                                    color: Colors.blue,
+                                    decoration: TextDecoration.underline,
+                                  ),
+                                ),
+                                TextSpan(text: ' and '),
+                                TextSpan(
+                                  text: 'Privacy Policy',
+                                  style: TextStyle(
+                                    color: Colors.blue,
+                                    decoration: TextDecoration.underline,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
                         ),
                       ),
                     ],
                   ),
-                ),
-
-                const SizedBox(height: 16),
-
-                SignInButton(
-                  Buttons.google,
-                  text: "Sign in with Google",
-                  onPressed: () {
-                    //TODO: signin btn logic
-                  },
-                ),
-
-                const SizedBox(height: 16),
-
-                // Checkbox with Terms of Service and Privacy Policy
-                Row(
-                  children: [
-                    Checkbox(
-                      value: _agreedToTerms,
-                      onChanged: (bool? value) {
-                        setState(() {
-                          _agreedToTerms = value ?? false;
-                        });
-                      },
-                    ),
-                    Expanded(
-                      child: GestureDetector(
-                        onTap: () {
-                          print("Show Policies");
-                        },
-                        child: RichText(
-                          text: const TextSpan(
-                            text: 'I agree to the ',
-                            style: TextStyle(color: Colors.black),
-                            children: [
-                              TextSpan(
-                                text: 'Terms of Service',
-                                style: TextStyle(
-                                  color: Colors.blue,
-                                  decoration: TextDecoration.underline,
-                                ),
-                              ),
-                              TextSpan(text: ' and '),
-                              TextSpan(
-                                text: 'Privacy Policy',
-                                style: TextStyle(
-                                  color: Colors.blue,
-                                  decoration: TextDecoration.underline,
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-              ],
+                ],
+              ),
             ),
           ),
         ),
