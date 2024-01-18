09cc071714b20cc0cda931377a995520c93f6470
ChandulaJ <114409648+ChandulaJ@users.noreply.github.com>
2024-01-18 10:49:21 +0530

    firebase cart is fetched but details not uploaded

--- lib/customerSide/controller/cart_page_controller.dart
+++ lib/customerSide/controller/cart_page_controller.dart
@@ -5,28 +5,33 @@ import '../data/repositories/cart_products_repository.dart';
 import '../models/product_data_model.dart';
 
 class CartPageController {
-  final CartProductsRepository _cart_repository = CartProductsRepository();
-  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+  final CartProductsRepository _cartRepository = CartProductsRepository();
+  final RxList<ProductDataModel> cartItems = <ProductDataModel>[].obs;
 
-  final RxList<ProductDataModel> cartItems = <ProductDataModel>[
-   ].obs;
-  final RxInt selectedTabIndex = 0.obs;
-
-  List<ProductDataModel> getCartItems() {
-    return cartItems;
+  Future<void> addToCart(ProductDataModel product) async {
+    try {
+      await _cartRepository.addToCart(product);
+      fetchCartData();
+    } catch (e) {
+      print("Error adding to cart: $e");
+    }
   }
+
+  Future<void> removeFromCart(ProductDataModel product) async {
+    try {
+      await _cartRepository.removeFromCart(product);
+      fetchCartData();
+    } catch (e) {
+      print("Error removing from cart: $e");
+    }
+  }
+
   Future<void> fetchCartData() async {
     try {
-      print("fetching cart data");
-      List<ProductDataModel> fetchedItems =
-      await _cart_repository.fetchCartItems();
+      List<ProductDataModel> fetchedItems = await _cartRepository.fetchCartItems();
       cartItems.assignAll(fetchedItems);
     } catch (e) {
       print("Error fetching cart items: $e");
     }
-  }
-
-  void setSelectedTabIndex(int index) {
-    selectedTabIndex.value = index;
   }
 }
